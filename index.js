fc4489c5527cc117c258df05f5dd0e260ffa31a3
akshath08 <68785070+akshath08@users.noreply.github.com>
2023-08-16 14:02:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var randomNumber1 = Math.floor((Math.random() * 6)+1);
 
 var randomDiceImage = "dice" + randomNumber1 + ".png";
 
-var randomImageSource1 = "images/" + randomDiceImage;
+var randomImageSource1 = randomDiceImage;
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -10,7 +10,7 @@ image1.setAttribute("src" , randomImageSource1);
 
 var randomNumber2 = Math.floor((Math.random() * 6) + 1);
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src" , randomImageSource2);
 
